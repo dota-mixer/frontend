63cecd9ea0c3250833154ff2f45c22464a3f8b13
TemaDrakoshef <artem125149@gmail.com>
2025-05-17 20:24:00 +0300

    Create heroes table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from '@/components/ui/button'
+import { TopHeroesTable } from '@/components/heroes-table'
 
 export default function Home() {
 	return (
-		<main className='h-screen flex flex-col items-center gap-2 p-4'>
-			<h1 className='text-3xl font-bold'>Hello world!</h1>
-			<Button variant='default'>Hello</Button>
+		<main className='flex flex-col items-center justify-between gap-4 pt-4'>
+			<h1 className='text-2xl font-bold'>Top Heroes</h1>
+			<TopHeroesTable />
 		</main>
 	)
 }
---
+++ src/components/heroes-table.tsx
@@ -0,0 +1,52 @@
+import { HeroesService } from '@/client'
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from '@/components/ui/table'
+import Image from 'next/image'
+
+export async function TopHeroesTable() {
+	const { data } = await HeroesService.getHeroes()
+
+	return (
+		<div className='h-[calc(100vh-192px)] overflow-auto w-full md:w-3/4 mx-auto bg-sidebar rounded-md '>
+			<Table className='w-full'>
+				<TableHeader>
+					<TableRow>
+						<TableHead>Hero</TableHead>
+						<TableHead>Winrate</TableHead>
+						<TableHead>Matches</TableHead>
+						<TableHead>Rating</TableHead>
+					</TableRow>
+				</TableHeader>
+				<TableBody>
+					{data.map((hero) => (
+						<TableRow key={hero.hero_id}>
+							<TableCell className='font-medium'>
+								<div className='flex items-center gap-2 hover:bg-secondary p-2 rounded-md'>
+									<Image
+										src={`https://dota2protracker.com/static/hero_images_jpg_res/${hero.npc}_lg.jpg`}
+										alt={hero.name}
+										className='rounded-md'
+										width={70}
+										height={70}
+									/>
+									<span>{hero.name}</span>
+								</div>
+							</TableCell>
+							<TableCell>
+								{Math.round((hero.all_winrate ?? 0) * 1000) / 10}%
+							</TableCell>
+							<TableCell>{hero.all_matches}</TableCell>
+							<TableCell>{hero.all_elo || 'N/A'}</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</div>
+	)
+}
