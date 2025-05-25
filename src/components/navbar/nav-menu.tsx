756ee97650d0970f9026213ad56db0afa8b04842
TemaDrakoshef <artem125149@gmail.com>
2025-05-25 22:14:13 +0300

    Add icons and active link display in navbar

--- src/components/navbar/nav-menu.tsx
+++ src/components/navbar/nav-menu.tsx
@@ -7,30 +7,44 @@ import {
 	NavigationMenuList,
 } from '@/components/ui/navigation-menu'
 import { NavigationMenuProps } from '@radix-ui/react-navigation-menu'
+import { BarChart3, Home, Users } from 'lucide-react'
 import { useTranslations } from 'next-intl'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+const navItems = [
+	{ href: '/', labelKey: 'home', Icon: Home },
+	{ href: '/meta', labelKey: 'meta', Icon: BarChart3 },
+	{ href: '/heroes', labelKey: 'heroes', Icon: Users },
+]
 
 export const NavMenu = (props: NavigationMenuProps) => {
 	const t = useTranslations('Nav')
+	const pathname = usePathname().slice(3) || '/'
 
 	return (
 		<NavigationMenu {...props}>
 			<NavigationMenuList className='gap-6 space-x-0 data-[orientation=vertical]:flex-col data-[orientation=vertical]:items-start'>
-				<NavigationMenuItem>
-					<NavigationMenuLink asChild>
-						<Link href='/'>{t('home')}</Link>
-					</NavigationMenuLink>
-				</NavigationMenuItem>
-				<NavigationMenuItem>
-					<NavigationMenuLink asChild>
-						<Link href='/meta'>{t('meta')}</Link>
-					</NavigationMenuLink>
-				</NavigationMenuItem>
-				<NavigationMenuItem>
-					<NavigationMenuLink asChild>
-						<Link href='/heroes'>{t('heroes')}</Link>
-					</NavigationMenuLink>
-				</NavigationMenuItem>
+				{navItems.map(({ href, labelKey, Icon }) => {
+					const isActive = pathname === href
+					return (
+						<NavigationMenuItem key={href}>
+							<NavigationMenuLink asChild active={isActive} className=''>
+								<Link
+									href={href}
+									className={`flex flex-row items-center gap-2 ${
+										isActive
+											? 'bg-accent font-medium focus-visible:transform'
+											: ''
+									}`}
+								>
+									<Icon className={`h-4 w-4`} />
+									{t(labelKey)}
+								</Link>
+							</NavigationMenuLink>
+						</NavigationMenuItem>
+					)
+				})}
 			</NavigationMenuList>
 		</NavigationMenu>
 	)
