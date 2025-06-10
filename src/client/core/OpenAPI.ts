770d90ddaa10e3131050aca27f4f720e423c7210
TemaDrakoshef <artem125149@gmail.com>
2025-06-11 00:01:50 +0300

    Update OpenAPI base URL to use environment variable

--- src/client/core/OpenAPI.ts
+++ src/client/core/OpenAPI.ts
@@ -41,7 +41,7 @@ export type OpenAPIConfig = {
 }
 
 export const OpenAPI: OpenAPIConfig = {
-	BASE: '',
+	BASE: process.env.NEXT_PUBLIC_API_BASE_URL || '/api',
 	CREDENTIALS: 'include',
 	ENCODE_PATH: undefined,
 	HEADERS: undefined,
